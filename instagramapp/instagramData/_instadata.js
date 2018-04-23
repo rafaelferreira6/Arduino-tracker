d061c900797ea330236c8c9b2a21d01c098dc0af
Your Name <rafaelferreira6@MBP-de-Rafael-3>
2018-04-23 17:46:49 +0100

    Tags return null

--- instagramapp/instagramData/_instadata.js
+++ instagramapp/instagramData/_instadata.js
@@ -1,17 +1,19 @@
 import {instagram} from './_instagram_auth';
 
+//Get Data User
 const getDataUser = access_token => {
   instagram.get('users/self', { access_token: access_token }, (err, data) => {
     //setInterval(function(){console.log(data)},10000);
   });
 }
 
+//Get Data by Tags
 const getDataTags = (access_token, tagName) => {
   instagram.get('tags/'+tagName+'/media/recent', { access_token: access_token}, (err, data) => {
-    if(data.pagination.data)
-    console.log(data);
+    if(data.data.length>0)
+    console.log(data)
     else
-    console.log("No_Data");
+    console.log("No-Data");
   });
 }
 
--- instagramapp/script.js
+++ instagramapp/script.js
@@ -2,5 +2,5 @@ import {getDataUser, getDataTags} from './instagramData/_instadata';
 
 export default function(_token){
   //getDataUser(_token);
-  getDataTags(_token, 'bn1m');
+  getDataTags(_token, 'bnm');
 }
